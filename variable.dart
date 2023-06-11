3cc7ce1a3154b6925c0c21e8d16cffa62f2eff14
devfaoed <bashirhaadiy@gmail.com>
2023-06-11 23:13:33 +0100

    understanding dart variable declarion

---
+++ variable.dart
@@ -0,0 +1,26 @@
+void main() {
+  // Variable
+
+  // strings
+  // var name = "Basheer";
+  String name = "Haadi";
+
+  // integer
+  var x = 34;
+
+  // int x = 34;
+
+  // Dynamic
+  dynamic myName = "Haadi";
+
+  // const and Final
+
+  const String firstName = "Basheer";
+  final String lastName = "Haadi";
+
+  // declaring nothing in a variable
+
+  var age;
+
+  print(firstName);
+}
