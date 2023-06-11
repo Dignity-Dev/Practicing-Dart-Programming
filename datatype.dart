d7f204cfda816db839153a4e982ddb66e819d84a
devfaoed <bashirhaadiy@gmail.com>
2023-06-11 23:14:36 +0100

    learn dart data types and explore it uses

---
+++ datatype.dart
@@ -0,0 +1,19 @@
+void main() {
+  // data type
+
+  // Numbers, Strings, Booleans, Lists, Maps, Dynamics
+
+  // Strings
+  String firstName = "Basheer";
+  print(firstName);
+  // print with concertinaltion
+  print("String; $firstName");
+
+  // integers
+  var age = 25;
+  print("Integer: $age");
+
+  // Doubles
+  double otherNum = 12.56778;
+  print("Double: $otherNum");
+}
