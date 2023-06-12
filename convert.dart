0d3a6567d37280bef4fd15f6377ac2e340b327c6
devfaoed <bashirhaadiy@gmail.com>
2023-06-12 21:12:27 +0100

    Conversion of of value from one data type to another

--- convert.dart
+++ convert.dart
@@ -6,5 +6,11 @@ void main() {
   b = "1";
   c = a + int.parse(b);
   print("$a + $b = $c");
+// String to double
 
+  var d, e, f;
+  d = 40;
+  e = "0.1";
+  f = d + double.parse(e);
+  print("$d + $e = $f");
 }
