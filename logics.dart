4d78b35f87b4483338a1da419975db93e92abe3f
devfaoed <bashirhaadiy@gmail.com>
2023-06-12 08:59:20 +0100

    Conditional operation for logic in dart programming

--- logics.dart
+++ logics.dart
@@ -6,10 +6,9 @@ void main() {
   var num = 3;
   if (num == 5) {
     print("the number is 5!");
-  } else if (num == 3){
-    print("Number is 3")
-  }
-  else {
+  } else if (num == 3) {
+    print("Number is 3");
+  } else {
     print("the number is not 5!");
   }
 }
