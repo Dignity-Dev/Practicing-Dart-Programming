596a910aff1c055ce389fc36b3cbb2b053f2f11d
devfaoed <bashirhaadiy@gmail.com>
2023-06-12 09:17:30 +0100

    Handling user input in dart programming

--- input.dart
+++ input.dart
@@ -5,5 +5,8 @@ void main() {
   print("Enter your Name: ");
 
   // allow the user to enter their name
-  var name = stdin.readLineSync();
+  // var name = stdin.readLineSync();
+
+  String? name = stdin.readLineSync();
+  print("Hello $name");
 }
